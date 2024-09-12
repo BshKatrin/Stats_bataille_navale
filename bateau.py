9a40685159c2fb2c6250af35a37c073f375b8bbb
franciline <ameliechu@outlook.com>
2024-09-12 23:22:19 +0200

    Modification et test des fonctions

--- bateau.py
+++ bateau.py
@@ -1,2 +1,7 @@
-BAT_CASE = {}
-#fichier temporaire à remplacer par le tien
+PORTE_AVION: int = 1
+CROISEUR: int = 2
+CONTRE_TORPILLEURS: int = 3
+SOUS_MARIN: int = 4
+TORPILLEUR: int = 5
+
+BAT_CASES = {PORTE_AVION: 5, CROISEUR: 4, CONTRE_TORPILLEURS: 3, SOUS_MARIN: 3, TORPILLEUR: 2}
--- grille2.py
+++ grille2.py
@@ -1,19 +1,19 @@
 import numpy
-from random import randint
-from bateau import BAT_CASE
 
+from random import randint
+from bateau import BAT_CASES
 
 class Grille:
     def __init__(self, n:int)->None:
+        self.n: int = n
         self.grille = numpy.zeros((n,n), dtype = numpy.int8)
     
 
-    def peut_placer(self, grille, bateau: int, position: tuple, direction: int)-> bool:
+    def peut_placer(self, bateau: int, position: tuple, direction: int)-> bool:
         """
         Renvoie True si peut placer le bateau sur la grille, False sinon.
 
         Args:
-            grille: n*n à valeurs [0, 5]
             bateau: [1, 5]
             position: tuple (ligne, col)
                 la position concerne la partie la plus a gauche ou la partie la plus en haut du bateau
@@ -21,23 +21,22 @@ class Grille:
         """
 
         ligne, col = position 
-        taille_g = len(grille)
-        taille_bat = BAT_CASE[bateau]   
+        taille_bat = BAT_CASES[bateau]   
 
         #cas horizontale
         if direction == 1:
 
             #cas tout a droite
-            if ligne == taille_g-1: 
+            if ligne == self.n - 1: 
                 return False
 
             #on verifie si il y a de la place pour le bateau
-            if taille_g - col < taille_bat:
+            if self.n - col < taille_bat:
                 return False
 
             #on verifie si les cases sont libres
             for i in range(col, col + taille_bat):
-                if grille[ligne][i] != 0:
+                if self.grille[ligne][i] != 0:
                     return False
             
             return True
@@ -47,27 +46,26 @@ class Grille:
         if direction == 2:
 
             #cas tout en bas
-            if col == taille_g-1: 
+            if col == self.n-1: 
                 return False
 
             #on verifie si il y a de la place pour le bateau
-            if taille_g - ligne < BAT_CASE[bateau]:
+            if self.n - ligne < BAT_CASES[bateau]:
                 return False
             
             #on verifie si les cases sont libres
             for i in range(ligne, ligne + taille_bat):
-                if grille[i][col] != 0:
+                if self.grille[i][col] != 0:
                     return False
             
             return True
 
 
-    def place(self, grille, bateau: int, position: tuple, direction: int):
+    def place(self, bateau: int, position: tuple, direction: int):
         """
         Renvoie la grille avec le bateau placé dessus.
 
         Args:
-            grille: n*n à valeurs [0, 5]
             bateau: [1, 5]
             position: tuple (ligne, col)
                 la position concerne la partie la plus a gauche ou la partie la plus en haut du bateau
@@ -75,37 +73,44 @@ class Grille:
         """
 
         ligne, col = position
-        taille_bat = BAT_CASE[bateau] 
+        taille_bat = BAT_CASES[bateau] 
 
         #cas horizontale
         if direction == 1:
             for i in range(col, col + taille_bat):
-                grille[ligne][i] = bateau
+                self.grille[ligne][i] = bateau
 
         #cas verticale
         if direction == 2:
             for i in range(ligne, ligne + taille_bat):
-                grille[i][col] = bateau
+                self.grille[i][col] = bateau
         
-        return grille
+        return self.grille
 
 
-    def place_alea(self, grille, bateau: int)-> None:
+    def place_alea(self, bateau: int)-> None:
         """Place le bateau aléatoirement dans la grille."""
-
-        taille = len(grille)
     
         while True:
-            position = (randint(0, taille), randint(0, taille))
+            position = (randint(0, self.n), randint(0, self.n))
             direction = randint(1,2)
 
-            if self.peut_placer(self.grille, bateau, position, direction) == True:
-                self.grille = self.place(grille, bateau, position, direction)
+            if self.peut_placer(bateau, position, direction) == True:
+                self.grille = self.place(bateau, position, direction)
                 break
         
         return
 
 
+if __name__ == "__main__":
+    g = Grille(10)
+    assert(g.peut_placer(5, (0,0), 1)) == True
+    g.place(5, (0,0), 1)
+    assert(g.peut_placer(5, (0,0), 1)) == False
+    assert(g.peut_placer(5, (0,1), 1)) == False
+    assert(g.peut_placer(5, (0,2), 1)) == True
+
+    g.place_alea(3)
             
 
 
