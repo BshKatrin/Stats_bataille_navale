f846a2086b6a7c3d0bb1ae8fefbdb83dd90ae51d
franciline <ameliechu@outlook.com>
2024-10-09 16:21:53 +0200

    modification version heuristique et qlq fichiers

--- data/calc_data.py
+++ data/calc_data.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import numpy as np
-from joueur import Joueur
+from Joueur import Joueur
 
 # DATA GENERATION
 if __name__ == '__main__':
     j = Joueur("Joueur")
     resultats_jeux = np.array([0] * 101)
 
-    for i in range(1000):
-        print(i)
-        nb_coup = j.jouer_proba_simple(10)
+    for i in range(10000):
+        nb_coup = j.jouer_heuristique(10)
+        print(i, nb_coup)
         # nb_coup = j.jouer_strat(10)
         resultats_jeux[nb_coup] += 1
 
     df = pd.DataFrame(resultats_jeux)
-    df.to_csv('data3.csv')
+    df.to_csv('data_1.csv')
